b8b23d6c19fdc7ee4cdfd6f49b4675b776540a90
Gerrit Jansen van Vuuren <gerritjvv@gmail.com>
2014-09-07 15:08:36 +0200

    minor test -- more needs to be added

---
+++ test/pjson/core_test.clj
@@ -0,0 +1,11 @@
+(ns pjson.core-test
+  (:require [clojure.test :refer :all]
+            [pjson.core :refer :all]))
+
+
+(deftest test-string-escaping
+
+  (is (=
+        (-> "{\"a\":\"1 \\\"  \"}"
+            asObj asString asObj asString asObj)
+        {"a" "1 \\"})))
