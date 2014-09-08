f1aafd01b943a327ebe85338bc2de67c1ae2434e
Gerrit Jansen van Vuuren <gerritjvv@gmail.com>
2014-09-08 11:22:55 +0200

    Fix types to string bug

--- src/main/java/pjson/JSONGenerator.java
+++ src/main/java/pjson/JSONGenerator.java
@@ -1,9 +1,6 @@
 package pjson;
 
-import clojure.lang.AFn;
-import clojure.lang.PersistentArrayMap;
-import clojure.lang.PersistentHashMap;
-import clojure.lang.PersistentVector;
+import clojure.lang.*;
 
 import java.io.IOException;
 import java.util.Collection;
@@ -12,149 +9,184 @@ import java.util.List;
 import java.util.Map;
 
 /**
- (import 'pjson.JSONGenerator)
- (require '[cheshire.core :as cheshire])
- (def msg (-> "test-resources/msg.json" slurp cheshire/parse-string))
- (dotimes [i 1000000]
- (JSONGenerator/forObj msg))
-
-
-  Performance tests: Goal < 800ms
-     StringWriter == 1.8 sec
-     Using internal byte array and casting char arrays to byte[]
-            -> no numbers 1.6 sec
-            -> no string creation in toString 1.5 sec
-            -> only iteration over data structures, no writing 1.0 sec
-            -> only iterate over Map, List and Collection, no writing or other iterations 668.252724 ms
-            -> iterate using kvmap 488.328465ms NOOP
-            -> iterate with byte array creating in UTF8JSONGenerator 724.631374 ms
-
+ * (import 'pjson.JSONGenerator)
+ * (require '[cheshire.core :as cheshire])
+ * (def msg (-> "test-resources/msg.json" slurp cheshire/parse-string))
+ * (dotimes [i 1000000]
+ * (JSONGenerator/forObj msg))
+ * <p/>
+ * <p/>
+ * Performance tests: Goal < 800ms
+ * StringWriter == 1.8 sec
+ * Using internal byte array and casting char arrays to byte[]
+ * -> no numbers 1.6 sec
+ * -> no string creation in toString 1.5 sec
+ * -> only iteration over data structures, no writing 1.0 sec
+ * -> only iterate over Map, List and Collection, no writing or other iterations 668.252724 ms
+ * -> iterate using kvmap 488.328465ms NOOP
+ * -> iterate with byte array creating in UTF8JSONGenerator 724.631374 ms
  */
 public final class JSONGenerator {
 
 
-    public static final String forObj(Object obj) throws IOException{
-        JSONWriter writer = new JSONWriter.StringBuilderWriter();
-
+    public static final String forObj(final Object obj) throws IOException {
+        final JSONWriter writer = new JSONWriter.StringBuilderWriter();
         forObj(writer, obj);
-
         return writer.toString();
     }
 
-    public static final void forObj(final JSONWriter writer, Object obj) throws IOException {
-       if(obj == null){
+    public static final void forObj(final JSONWriter writer, final Object obj) throws IOException {
+        if (obj == null) {
             writer.writeNull();
-       }else{
+        } else {
+            if (obj instanceof String) {
+                writer.writeString((String) obj);
+            } else if (obj instanceof Named) {
+                //support keywords and symbols
+                writer.writeString(((Named) obj).getName());
+            } else if (obj instanceof Integer) {
+                writer.writeInt((Integer) obj);
+            } else if (obj instanceof Long) {
+                writer.writeLong((Long) obj);
+            } else if (obj instanceof Double) {
+                writer.writeDouble((Double) obj);
+            } else if (obj instanceof Float) {
+                writer.writeFloat((Float) obj);
+            } else if (obj instanceof ToJSONString) {
+                ((ToJSONString) obj).toString(writer);
+            } else if (obj instanceof PersistentArrayMap) {
+                writer.startObj();
+                final PersistentArrayMap map = (PersistentArrayMap) obj;
 
-           if(obj instanceof String) {
-               writer.writeString((String)obj);
-           }
-           else if(obj instanceof Integer) {
-               writer.writeInt((Integer)obj);
-           }else if(obj instanceof Long) {
-               writer.writeLong((Long)obj);
-           }else if(obj instanceof Double) {
-               writer.writeDouble((Double)obj);
-           }else if(obj instanceof Float) {
-               writer.writeFloat((Float)obj);
-           }else if(obj instanceof ToJSONString) {
-               ((ToJSONString)obj).toString(writer);
-           }else if(obj instanceof PersistentArrayMap) {
-               PersistentArrayMap map = (PersistentArrayMap)obj;
+                map.kvreduce(new AFn() {
+                    @Override
+                    public Object invoke(Object state, Object key, Object val) {
+                        if (((Counter) state).i++ != 0)
+                            writer.writeComma();
 
-               map.kvreduce(new AFn() {
-                   @Override
-                   public Object invoke(Object state, Object key, Object val) {
-                       if(((Counter)state).i++ != 0)
-                           writer.writeComma();
+                        writer.writeFieldName(convertFieldName(key));
 
-                       writer.writeFieldName(key.toString());
+                        try {
+                            JSONGenerator.forObj(writer, val);
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
 
-                       try{
-                           JSONGenerator.forObj(writer, val);
-                       }catch(Exception e){
-                           throw new RuntimeException(e);
-                       }
+                        return state;
+                    }
+                }, new Counter());
+                writer.endObj();
+            } else if (obj instanceof PersistentHashMap) {
+                writer.startObj();
+                final PersistentHashMap map = (PersistentHashMap) obj;
+                map.kvreduce(new AFn() {
+                    @Override
+                    public Object invoke(Object state, Object key, Object val) {
+                        if (((Counter) state).i++ != 0)
+                            writer.writeComma();
 
-                       return state;
-                   }
-               }, new Counter());
-           }else if(obj instanceof PersistentHashMap) {
-               PersistentHashMap map = (PersistentHashMap)obj;
-               map.kvreduce(new AFn() {
-                   @Override
-                   public Object invoke(Object state, Object key, Object val) {
-                       if(((Counter)state).i++ != 0)
-                           writer.writeComma();
+                        writer.writeFieldName(convertFieldName(key.toString()));
 
-                       writer.writeFieldName(key.toString());
+                        try {
+                            JSONGenerator.forObj(writer, val);
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
 
-                       try{
-                           JSONGenerator.forObj(writer, val);
-                       }catch(Exception e){
-                           throw new RuntimeException(e);
-                       }
+                        return state;
+                    }
+                }, new Counter());
+                writer.endObj();
+            } else if (obj instanceof PersistentVector) {
+                writer.startArr();
+                ((PersistentVector) obj).kvreduce(new AFn() {
+                    @Override
+                    public Object invoke(Object state, Object k, Object val) {
+                        if (((Counter) state).i++ != 0)
+                            writer.writeComma();
 
-                       return state;
-                   }
-               }, new Counter());
-           }else if(obj instanceof PersistentVector) {
-               ((PersistentVector)obj).kvreduce(new AFn() {
-                   @Override
-                   public Object invoke(Object state, Object k, Object val) {
-                       if(((Counter)state).i++ != 0)
-                           writer.writeComma();
+                        try {
+                            JSONGenerator.forObj(writer, val);
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
 
-                       try{
-                           JSONGenerator.forObj(writer, val);
-                       }catch(Exception e){
-                           throw new RuntimeException(e);
-                       }
+                        return state;
+                    }
+                }, new Counter());
+                writer.endArr();
+            } else if (obj instanceof Map) {
+                writer.startObj();
+                int i = 0;
+                for (Map.Entry<Object, Object> entry : ((Map<Object, Object>) obj).entrySet()) {
+                    if (i++ != 0)
+                        writer.writeComma();
 
-                       return state;
-                   }
-               }, new Counter());
-           }else if(obj instanceof Map){
-               writer.startObj();
-               int i = 0;
-               for(Map.Entry<Object, Object> entry : ((Map<Object, Object>)obj).entrySet()){
-                   if(i++ != 0)
-                       writer.writeComma();
+                    writer.writeFieldName(convertFieldName(entry.getKey()));
+                    forObj(writer, entry.getValue());
+                }
 
-                   writer.writeFieldName(entry.getKey().toString());
-                   forObj(writer, entry.getValue());
-               }
+                writer.endObj();
 
-               writer.endObj();
+            } else if (obj instanceof List) {
+                final List<Object> list = (List<Object>) obj;
+                writer.startArr();
+                for (int i = 0; i < list.size(); i++) {
+                    if (i != 0)
+                        writer.writeComma();
 
-           }else if(obj instanceof List){
-               List<Object> list = (List<Object>)obj;
+                    forObj(writer, list.get(i));
+                }
+                writer.endArr();
+            } else if (obj instanceof Iterable) {
+                final Iterator<Object> coll = ((Iterable<Object>) obj).iterator();
+                int i = 0;
 
-               for(int i = 0; i < list.size(); i++){
-                   if(i != 0)
-                       writer.writeComma();
+                writer.startArr();
+                while (coll.hasNext()) {
+                    if (i++ != 0)
+                        writer.writeComma();
+                    forObj(writer, coll.next());
+                }
 
-                   forObj(writer, list.get(i));
-               }
-
-           }else if(obj instanceof Collection){
-               Iterator<Object> coll = ((Collection<Object>)obj).iterator();
-               int i = 0;
-
-               writer.startArr();
-               while(coll.hasNext()){
-                   if(i++ != 0)
-                       writer.writeComma();
-                   forObj(writer, coll.next());
-               }
-
-               writer.endArr();
-           }
-       }
+                writer.endArr();
+            }else {
+                writer.writeString(cleanUnknownOutput(obj.toString()));
+            }
+        }
     }
 
-    private static final class Counter{
+    private final static String convertFieldName(Object key) {
+        if (key instanceof String)
+            return (String) key;
+        else if (key instanceof Named)
+            return ((Named) key).getName();
+        else
+            return key.toString();
+    }
+
+    /**
+     * Used to cleanout any unknown objects that would most certainly contain illegal characters.
+     *
+     * @param s
+     * @return
+     */
+    private final static String cleanUnknownOutput(String s) {
+        char[] chrs = StringUtil.toCharArray(s);
+        char[] newchrs = new char[chrs.length];
+        for (int i = 0; i < chrs.length; i++) {
+            char ch = chrs[i];
+            if (ch == '[' || ch == ']' || ch == '{' || ch == '}' || ch == '"')
+                newchrs[i] = '_';
+            else
+                newchrs[i] = chrs[i];
+
+        }
+
+        return StringUtil.noCopyStringFromChars(newchrs);
+    }
+
+    private static final class Counter {
         int i = 0;
     }
-
 }
--- test/pjson/core_test.clj
+++ test/pjson/core_test.clj
@@ -1,8 +1,12 @@
 (ns pjson.core-test
   (:require [clojure.test :refer :all]
             [pjson.core :refer :all])
-  (:import [pjson CharArrayTool]))
+  (:import [pjson CharArrayTool]
+           [java.util HashMap ArrayList LinkedList TreeSet]))
 
+
+(defn equal-after-decode [m1 m2]
+  (= (-> m1 write-str read-str) m2))
 
 (deftest test-string-escaping
   (let [test-str "[\"mystring\"]"]
@@ -27,3 +31,21 @@
   (is (= (read-str "[\"a\", \"b\"]") ["a" "b"]))
   (is (= (read-str "[\"a\", \"b\", [\"c\"]]") ["a" "b" ["c"]]))
   (is (= (read-str "[\"a\", \"b\", [\"c\"], {\"d\": 1}]") ["a" "b" ["c"] {"d" 1}])))
+
+
+(deftest test-keywords
+  (is (= (write-str {:a 1 'b 2})  "{\"b\":2,\"a\":1}")))
+
+
+(deftest test-types-to-string
+  (is (equal-after-decode (array-map "a" 1 "b" 2) {"a" 1 "b" 2})) ;test PersistentArrayMap
+  (is (equal-after-decode (hash-map "a" 1 "b" 2) {"a" 1 "b" 2})) ;test PersistentHashMap
+  (is (equal-after-decode (vector 1 2 3) [1 2 3]))          ;test PersistentVector
+  (is (equal-after-decode (sequence [1 2 3]) [1 2 3]))          ;test Sequence
+
+  (is (equal-after-decode (doto (HashMap.) (.put "a" 1) (.put "b" 2)) {"a" 1 "b" 2})) ;test HashMap
+  (is (equal-after-decode (doto (ArrayList.) (.add 1.2) (.add 2.3)) [1.2 2.3])) ;test List
+  (is (equal-after-decode (doto (LinkedList.) (.add 1.2) (.add 2.3)) [1.2 2.3])) ;test Collection/Iterable
+  (is (equal-after-decode (doto (TreeSet.) (.add 1.2) (.add 2.3)) [1.2 2.3])) ;test Collection/Iterable
+
+  )
