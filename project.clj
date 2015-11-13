f7feced1aef8b9dd30fb494cc727a99c8480dc0a
Gerrit Jansen van Vuuren <gerritjvv@gmail.com>
2015-11-13 12:21:25 +0100

    up version for release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pjson "0.3.1-SNAPSHOT"
+(defproject pjson "0.3.1"
             :description "Fast clojure json library"
             :url "https://github.com/gerritjvv/pjson"
             :license {:name "Eclipse Public License"
