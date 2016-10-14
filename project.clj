de95caf86dfc083644700fd6d0d4b4b56186a535
Gerrit Jansen van Vuuren <gerritjvv@gmail.com>
2016-10-14 16:26:16 +0200

    update to version 8

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pjson "0.3.7"
+(defproject pjson "0.3.8"
             :description "Fast clojure json library"
             :url "https://github.com/gerritjvv/pjson"
             :license {:name "Eclipse Public License"
